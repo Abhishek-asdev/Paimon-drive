3a02dab2bcdd3ee9461584a841647390c7721415
ay286677ty <ay286677ty@gmail.com>
2025-04-04 14:29:57 +0530

    first commit for paimon

---
+++ components/Header.jsx
@@ -0,0 +1,87 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { ArrowLeft, CarFront, Heart, Layout } from "lucide-react";
+
+const Header = async ({ isAdminPage = false }) => {
+  const isAdmin = false; // Replace with your logic to check if the user is an admin
+
+  return (
+    <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+      <nav className="mx-auto px-4 py-4 flex items-center justify-between">
+        <Link href={isAdminPage ? "/admin" : "/"} className="flex">
+          <Image
+            src={"/logop.png"}
+            alt="Paimon Drive Logo"
+            width={300}
+            height={60}
+            className="h-19 w-auto object-contain"
+          />
+          {isAdminPage && (
+            <span className="text-xl font-extralight">admin</span>
+          )}
+        </Link>
+        <div className="flex items-center space-x-4">
+          {isAdminPage ? (
+            <Link href="/" className="text-gray-700 hover:text-gray-900">
+              <Button variant="outline" className="flex items-center gap-2">
+                <ArrowLeft size={18} />
+                <span>Back to App</span>
+              </Button>
+            </Link>
+          ) : (
+            <SignedIn>
+              <Link
+                href="/saved-cars"
+                className="text-gray-700 hover:text-gray-900"
+              >
+                <Button>
+                  <Heart size={18} />
+                  <span className="hidden md:inline">Saved Cars</span>
+                </Button>
+              </Link>
+              {!isAdmin ? (
+                <Link
+                  href="/reservations"
+                  className="text-gray-700 hover:text-gray-900"
+                >
+                  <Button variant="outline">
+                    <CarFront size={18} />
+                    <span className="hidden md:inline">My Reservations</span>
+                  </Button>
+                </Link>
+              ) : (
+                <Link
+                  href="/admin"
+                  className="text-gray-700 hover:text-gray-900"
+                >
+                  <Button variant="outline">
+                    <Layout size={18} />
+                    <span className="hidden md:inline">Admin Portal</span>
+                  </Button>
+                </Link>
+              )}
+            </SignedIn>
+          )}
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/">
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+          </SignedOut>
+
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: { avatarBox: "w-10 h-10" },
+              }}
+            />
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
